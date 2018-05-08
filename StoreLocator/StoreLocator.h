75fe3123d9779ff128d26da73517405774edf5e5
Oleg Kirichenko <oleg.kirichenko@c02vnf27htd5.local>
2018-05-08 15:35:02 -0400

    Initial Commit

---
+++ StoreLocator/StoreLocator.h
@@ -0,0 +1,19 @@
+//
+//  StoreLocator.h
+//  StoreLocator
+//
+//  Created by Oleg Kirichenko on 2018-05-08.
+//  Copyright © 2018 Oleg Kirichenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StoreLocator.
+FOUNDATION_EXPORT double StoreLocatorVersionNumber;
+
+//! Project version string for StoreLocator.
+FOUNDATION_EXPORT const unsigned char StoreLocatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StoreLocator/PublicHeader.h>
+
+

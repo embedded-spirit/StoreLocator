0fd5a0c401e1123a0b16427a08a02f4937f8042d
Oleg Kirichenko <oleg.kirichenko@C02VNF27HTD5.Corp.ad.ctc>
2018-05-09 17:29:38 -0400

    table

--- StoreLocator/StoreViewController.swift
+++ StoreLocator/StoreViewController.swift
@@ -11,7 +11,8 @@ import Alamofire
 
 public class StoreViewController: UITableViewController {
 
-    let storeSearchURL = "https://stagemapi.cantire.net/mobile/sg/search?app_version=6.8&datetime=2018-5-9T11%3A45%3A18&device_type=ios&group_list=Canadian%2BTire%2BStore&locale=en_CA&model_type=iPhone7,2&os_version=11.3&page_size=25&query=M2n7c6"
+    var storeList = [String]()
+    
     public var searchTerm: String?
 
     override public func viewDidLoad() {
@@ -21,18 +22,34 @@ public class StoreViewController: UITableViewController {
         if let st = self.searchTerm {
 
             print("Search term is \(st)")
-
+            let storeSearchURL = "https://stagemapi.cantire.net/mobile/sg/search?app_version=6.8&datetime=2018-5-9T11%3A45%3A18&device_type=ios&group_list=Canadian%2BTire%2BStore&locale=en_CA&model_type=iPhone7,2&os_version=11.3&page_size=25&query=\(st)"
+            
             Alamofire.request(storeSearchURL).responseJSON { response in
 
-                if let json = response.result.value as? [String:Any] {
-                    print(json)
+                if let json = response.result.value as? [String:Any],
+                   let res = json["responsecollection"] as? [String:Any],
+                   let xs = res["responses"] as? [[String:String]] {
+                    print(xs)
+                    /*for x in xs {*/
+                        /*storeList.append(x["name"])*/
+                    /*}*/
+                    self.tableView.reloadData()
                 }
-
             }
         }
 
         self.title = "Select store"
 
+    }
+
+    override public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return storeList.count
+    }
+    
+    override public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Store", for: indexPath)
+        cell.textLabel?.text = storeList[indexPath.row]
+        return cell
     }
 
     override public func didReceiveMemoryWarning() {
